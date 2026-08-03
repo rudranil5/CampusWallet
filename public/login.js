abf24509fd0c1f972a4aa76386c350a87905f976
Rudranil <rudranilkhanra090@gmail.com>
2026-08-03 12:37:49 +0530

    fix

--- public/login.js
+++ public/login.js
@@ -22,7 +22,7 @@ async function sendData( )
 
    };
    try {
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
