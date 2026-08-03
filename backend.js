73f822b83275fd142fbd0810223a7e57005fad02
Rudranil <rudranilkhanra090@gmail.com>
2026-08-03 12:32:09 +0530

    fix

--- backend.js
+++ backend.js
@@ -688,4 +688,4 @@ console.log(data);
 app.use(express.static(path.join(__dirname, "public")));
 
 let PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on :${port}`));
+app.listen(PORT, () => console.log(`Server listening on :${PORT}`));
