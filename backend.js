190cc414eaaecdd7eb966bd296a06cb979860511
Rudranil <rudranilkhanra090@gmail.com>
2026-08-15 01:38:19 +0530

    .

--- backend.js
+++ backend.js
@@ -73,7 +73,7 @@ app.post("/register", (req, res) => {
    (err, result) => {
       if (err) {
         console.log(err);
-        if (err.sqlMessage==="Duplicate entry '?' for key 'details.PRIMARY'")
+        if (err.code === 'ER_DUP_ENTRY')
         {
           return res.json({message:"Id already Registered"});
         }
